a441e349be40b7fa833b42626a22bf68404d8677
Nicolas Bird von Eyben <nicolas.bird.voneyben@gmail.com>
2025-04-27 20:59:59 -0700

    update ok_default documentation

--- tests/ui/ok_default.rs
+++ tests/ui/ok_default.rs
@@ -2,20 +2,24 @@ use get_set_macro::get_set;
 
 #[get_set(default(inline_always), get)]
 struct Example {
+    // The function that this generates will still be #[inline(always)]
     #[gsflags(get(rename = "renamed_get_name"))]
     name: String,
 
     // Has noinline set, will override the global inline
+    // Equalivalent to 
+    // #[gsflags(get(noinline))]
     #[gsflags(default(noinline), get)]
     age: u32,
 
-    // Still recieves the global flags: get(inline_always)
+    // Inherits the following from global settings: 
+    // #[gsflags(default(inline_always), get)]
     unflagged: i64,
 
     #[gsflags(skip)]
     skipped: f32,
 
-    //
+    // Removes the `inline_always` default, get (`city_ref`) is #[inline(never)] while `set_city` has no inline attribute (because of the new default)
     #[gsflags(default(noinline), get(inline_never, rename = "city_ref"), set(rename = "set_city" /* same as default */))]
     city: String,
 }
