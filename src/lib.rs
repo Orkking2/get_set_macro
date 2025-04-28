3e08b3cff94ecd35d3fa4128a0b8f6106d5f3c36
Nicolas Bird von Eyben <nicolas.bird.voneyben@gmail.com>
2025-04-28 16:35:29 -0700

    remove random doctest from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -36,8 +36,6 @@
 //! - [More examples](https://github.com/Orkking2/get_set_proc_macro/tree/main/tests/ui)
 
 use proc_macro::TokenStream;
-#[cfg(test)]
-use quote::quote;
 use syn::{parse_macro_input, punctuated::Punctuated, Meta, Token};
 
 mod enums;
@@ -54,27 +52,4 @@ pub fn get_set(attr: TokenStream, item: TokenStream) -> TokenStream {
     parser::expand_get_set(gs_attrs, input)
         .unwrap_or_else(syn::Error::into_compile_error)
         .into()
-}
-
-#[test]
-fn debug() {
-    let quote = quote! {
-        // #[get_set] Removed by compiler
-        struct Example {
-            #[gsflags(get)]
-            name: String,
-
-            #[gsflags(get_copy)]
-            age: u32,
-
-            #[gsflags(get(inline_always, vis = "pub(crate)", rename = "city_ref"), set(rename = "set_city" /* same as default */))]
-            city: String,
-        }
-    };
-
-    let input = syn::parse2::<syn::ItemStruct>(quote).unwrap();
-
-    let parsed = parser::expand_get_set(None, input).unwrap_or_else(syn::Error::into_compile_error);
-
-    println!("parsed: {parsed}");
 }
